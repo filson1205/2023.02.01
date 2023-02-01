c1eec932284ba6e124be36dae5751abe6edfd13a
filson1205 <sondongfil1205@gmail.com>
2023-02-01 23:36:11 +0900

    복습

---
+++ src/com/greedy/section02/userexception/exception/PriceNegativeException.java
@@ -0,0 +1,10 @@
+package com.greedy.section02.userexception.exception;
+
+public class PriceNegativeException extends NegativeException{
+
+	public PriceNegativeException() {}
+	
+	public PriceNegativeException(String message) {
+		super(message);
+	}
+}
